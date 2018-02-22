8d422889f176e952bc5a41dd79043751da5fb69b
arieled91 <arieled91@gmail.com>
2018-02-22 03:24:59 -0300

    add menu tabs

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -8,12 +8,25 @@ import * as mdc from 'material-components-web'
 })
 export class AppComponent implements AfterContentInit{
   drawer: any;
-
+  active = 0;
 
   title = 'FUTBOLDESA';
 
+  constructor(){
+  }
+
+  menus = [
+    {name: "Home", link: "home"},
+    {name: "Partidos", link: "game"}
+  ];
+
   ngAfterContentInit(): void {
+    // this.setDevice();
     let drawer = new mdc.drawer.MDCTemporaryDrawer(document.querySelector('.mdc-drawer--temporary'));
     document.querySelector('.menu').addEventListener('click', () => drawer.open = true);
+  }
+
+  activate(index: number) {
+    this.active = index;
   }
 }
