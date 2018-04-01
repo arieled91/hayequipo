a528da46bc0b4ea948177259465d5d90ead68f33
arieled91 <arieled91@gmail.com>
2018-04-01 00:10:44 -0300

    add check files exist

--- webapp/deploy.sh
+++ webapp/deploy.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 #upload files
-npm run build &&
-aws s3 rm s3://futboldesa --recursive &&
-aws s3 cp ./dist s3://futboldesa --recursive --acl public-read
+npm run build
+if [ -e  ./dist/index.html ] # check index exists
+then
+  aws s3 rm s3://futboldesa --recursive
+  aws s3 cp ./dist s3://futboldesa --recursive --acl public-read
+fi
