5c909ef81fa5af312b84323d4c3e2082d5ff5027
Ariel Cacciamano <acacciamano@proveedores.garbarino.com.ar>
2018-04-04 11:37:47 -0300

    moved backend to its own module

---
+++ backend/src/main/java/org/arieled91/hayequipo/Application.java
@@ -0,0 +1,33 @@
+package org.arieled91.hayequipo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+@EnableScheduling
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+    @Value("${frontend.url}")
+    private final String frontend = "http://localhost:4200";
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins(frontend,"*")
+                        .allowedMethods("GET","POST","PATCH","PUT","DELETE")
+                        .allowedHeaders("*");
+			}
+		};
+	}
+}
