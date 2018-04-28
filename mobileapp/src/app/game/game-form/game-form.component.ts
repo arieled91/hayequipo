2dac3310ca1566b858589549397822706500276c
Ariel <arieled91@gmail.com>
2018-04-28 20:29:09 -0300

    replaced game popup for game form

--- mobileapp/src/app/game/game-form/game-form.component.ts
+++ mobileapp/src/app/game/game-form/game-form.component.ts
@@ -1,8 +1,9 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {fieldTypes, Game} from "../game.model";
 import {isNullOrUndefined} from "util";
 import {GameService} from "../service/game.service";
-import * as moment from 'moment';
+import {ActivatedRoute, Router} from "@angular/router";
+import {MatSnackBar} from "@angular/material";
 
 @Component({
   selector: 'app-game-form',
@@ -11,7 +12,7 @@ import * as moment from 'moment';
 })
 export class GameFormComponent implements OnInit {
 
-  @Input() id : Number = null;
+  @Input() id : number = null;
   title = "Partido";
 
   descriptionLabel = "Descripción";
@@ -23,39 +24,47 @@ export class GameFormComponent implements OnInit {
   game : Game = new Game();
   fields = fieldTypes;
 
-  @Output() onSaved = new EventEmitter<boolean>();
-  @Output() onCancel = new EventEmitter<void>();
+  // @Output() onSaved = new EventEmitter<boolean>();
+  // @Output() onCancel = new EventEmitter<void>();
 
-  constructor(private gameService: GameService) {
+  constructor(private gameService: GameService,
+              private router: Router,
+              private snackBar: MatSnackBar,
+              private route: ActivatedRoute
+  ) {
   }
 
   ngOnInit() {
-    if(!isNullOrUndefined(this.id)) this.gameService.findById(this.id).subscribe(data => this.game = data);
+    this.route.params.subscribe(params => {
+      this.id = +params['id'];
+    });
+
+    if(!isNullOrUndefined(this.id) && this.id > 0) this.gameService.findById(this.id).subscribe(data => this.game = data);
   }
 
   save() {
     this.gameService.saveGame(this.game).subscribe(
-      data => this.onSaved.emit(true)
+    data => {
+      this.router.navigate(["game-list"]);
+      this.router.navigate(["game"]);
+    },
+    error => this.snackBar.open(error)
     );
   }
 
-  cancel(){
-    this.onCancel.emit()
-  }
-
-  setTime(event){
-    let time = event;
-    this.game.dateTime = moment(this.game.dateTime).hours(isNullOrUndefined(time) ? 0 : event.substring(0,2));
-    this.game.dateTime = moment(this.game.dateTime).minutes(isNullOrUndefined(time) ? 0 : event.substring(3,5));
-    this.game.dateTime = moment(this.game.dateTime).seconds(0);
-    this.game.dateTime = moment(this.game.dateTime).milliseconds(0);
-
-    console.log(this.game.dateTime);
-  }
-
-  getGameTime(){
-    return isNullOrUndefined(this.game.dateTime) ? this.game.dateTime : moment(this.game.dateTime).format("HH:mm")
-  }
+  // setTime(event){
+  //   let time = event;
+  //   this.game.dateTime = moment(this.game.dateTime).hours(isNullOrUndefined(time) ? 0 : event.substring(0,2));
+  //   this.game.dateTime = moment(this.game.dateTime).minutes(isNullOrUndefined(time) ? 0 : event.substring(3,5));
+  //   this.game.dateTime = moment(this.game.dateTime).seconds(0);
+  //   this.game.dateTime = moment(this.game.dateTime).milliseconds(0);
+  //
+  //   console.log(this.game.dateTime);
+  // }
+  //
+  // getGameTime(){
+  //   return isNullOrUndefined(this.game.dateTime) ? this.game.dateTime : moment(this.game.dateTime).format("HH:mm")
+  // }
 
   getCapacityValue() {
     return this.fields.find(g => g.capacity==this.game.capacity)
